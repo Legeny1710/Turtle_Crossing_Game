4683e93b498adbc6b856a3cca9dbb6e3f739c15d
Yusuf Kula <yvk2045@gmail.com>
2023-08-03 12:41:37 +0100

    Initial setup

---
+++ main.py
@@ -0,0 +1,29 @@
+from turtle import Screen
+import  time
+from player import Player
+from score_board import ScoreBoard
+from car_manager import CarManager
+
+
+
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("Turtle Crossing Game")
+screen.tracer(0)
+
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+
+
+
+
+
+
+screen.exitonclick()
